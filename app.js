0d0610ef6184c8df71958ffdde6579e895fe3862
Sammii <sammii.h@icloud.com>
2019-11-04 12:03:37 +0000

    code tidy and finishing up, adding comments

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const currentDate = document.getElementById('currentDate')
   const timeFormat = document.getElementById('timeFormat')
   const dateFormat = document.getElementById('dateFormat')
+  const months = [
+    'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+  ]
+  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
   let date = null
   let time = null
@@ -18,33 +22,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const today = new Date()
     const mi = String(today.getMinutes()).padStart(2, '0')
     const ss = String(today.getSeconds()).padStart(2, '0')
-
     hh = today.getHours()
 
+    // Checks and toggles time format
     timeFormat.innerText = twentyFourHrs === true ? '12 hour clock' : '24 hour clock'
-
     time = hh + ':' + mi + ':' + ss
-
-    // Checks time format for AM/PM addition
     if (twentyFourHrs === false) {
       const amOrPm = hh >= 12 ? 'pm' : 'am'
       hh = (hh % 12)
       hh = hh !== 0 ? hh : 12
       time = hh + ':' + mi + ':' + ss + amOrPm
     }
-
-
     currentTime.innerText = time
   }
   setTime()
-
   setInterval(setTime, 1000)
 
-
-  const months = [
-    'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
-  ]
-  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
   const setDate = function() {
     const today = new Date()
@@ -53,10 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const yyyy = today.getFullYear()
     const dotw = today.getDay()
     let dayCap = null
-    // let dayCap = String(today.getDate())
-    // if (dayCap === 4) dayCap = 'th'
 
-    switch (today.getDate()) {
+    switch (dd) {
       case 1:
       case 21:
         dayCap = 'st'
@@ -97,23 +88,13 @@ document.addEventListener('DOMContentLoaded', () => {
         dayCap = 'th'
         break
     }
-
+    // Checks and toggles date format
     dateFormat.innerText = ddMmYyyy === true ? 'Day Month, Yr' : 'DD/MM/YY'
-
     if (ddMmYyyy === true) date = String(dd).padStart(2, '0') + '/' + mo + '/' + yyyy
-    // else date = dotw + ' ' + months[mo -1] + ', ' + yyyy
-
     else date = days[dotw] + ' ' + dd + dayCap + ' ' + months[mo -1] + ', ' + yyyy
     currentDate.innerText = date
-
-    // + dayCap
-
-    console.log(date)
-    console.log(days[dotw])
-
   }
   setDate()
-
 
 
   timeFormat.addEventListener('click', () => {
@@ -126,13 +107,5 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(ddMmYyyy)
     setDate()
   })
-
-
-
-
-
-
-
-
 
 })
