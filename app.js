1d6ce8df58faadc4764dccf1126a1bfbdee2034f
Sammii <sammii.h@icloud.com>
2019-11-04 00:24:52 +0000

    24/12 hr roggle functioning

--- app.js
+++ app.js
@@ -11,15 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
   let hh = null
   let twentyFourHrs = true
 
-  timeFormat.innerHTML = twentyFourHrs === true ? 'Set to 12hr Time Format' : 'Set to 24hr Time Format'
-  
-  timeFormat.addEventListener('click', () => {
-    twentyFourHrs = twentyFourHrs === false ? true : false
-
-    console.log(twentyFourHrs)
-    setClock()
-  })
-
 
   const setClock = function() {
     const today = new Date()
@@ -34,24 +25,31 @@ document.addEventListener('DOMContentLoaded', () => {
     date = mo + '/' + dd + '/' + yyyy
     time = hh + ':' + mi + ':' + ss
 
-
-
-
     // Checks time format for AM/PM addition
     if (twentyFourHrs === false) {
       const amOrPm = hh >= 12 ? 'pm' : 'am'
-      time += amOrPm
-      hh = (hh % 12) || 12
-      console.log(time)
+      hh = (hh % 12)
+      hh = hh !== 0 ? hh : 12
+      time = hh + ':' + mi + ':' + ss + amOrPm
     }
+
 
     currentDate.innerHTML = date
     currentTime.innerHTML = time
   }
-
   setClock()
 
   setInterval(setClock, 1000)
+
+
+  timeFormat.innerHTML = twentyFourHrs === true ? 'Set to 12hr Time Format' : 'Set to 24hr Time Format'
+
+  timeFormat.addEventListener('click', () => {
+    twentyFourHrs = twentyFourHrs === false ? true : false
+
+    console.log(twentyFourHrs)
+    setClock()
+  })
 
 
 
